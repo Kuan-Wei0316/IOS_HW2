7905dadebeb844426731bb50f6bac78adad9b48d
KuanWeiChen <chen20020316@gmail.com>
2022-10-19 23:51:14 +0800

    HW2建置

---
+++ HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW2
+//
+//  Created by KuanWei on 2022/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
